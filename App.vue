8cd1b6c1a3b22e7d7d6f9f86d6657ee49ec07500
kavion1024 <kavion1024@gmail.com>
2024-10-01 11:30:53 +0800

    1001更新檔

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #FFFCF6;
+  background-image: url('@/assets/HOME/背景.jpg');
+}
+</style>
---
+++ Product2.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container-fluid">
+        <div class="d-flex justify-content-center">
+            <router-link to="page"><img src="../../../assets/抹茶蜜柚蘭姆栗子/494007.jpg" alt="" class="img-fulid"></router-link>
+            <router-link to="page"><img src="../../../assets/梨山烏龍 芋泥 抹茶淋醬 酥餅/476236.jpg" alt="" class="img-fulid"></router-link>
+            <router-link to="page"><img src="../../../assets/水蜜桃布丁燒/461801_0.jpg" alt="" class="img-fulid"></router-link>
+        </div>
+        <div class="d-flex justify-content-center">
+            <router-link to="showbody"><button class="btn">觀看全部</button></router-link>
+        </div>
+    </div>
+</template>
+
+<style>
+    .img-fulid {
+        width: 200px;
+        height: 200px;
+        margin: 10px;
+    }
+
+    .btn {
+        background-color:mediumspringgreen; 
+        font-size: 20px;
+        width: 120px;
+        height: 60px;
+        opacity: 1;
+    }
+    .btn:hover {
+        opacity: 1;
+        background-color:mediumspringgreen; 
+    }
+</style>
+
+<script>
+export default {
+    name:'Product2'
+}
+</script>
